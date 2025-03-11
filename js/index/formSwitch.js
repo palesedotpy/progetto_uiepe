4951212d11b1925ff4e65be1e2cbd8600cd070c3
Palese <tdesantiss2006@gmail.com>
2025-03-11 23:22:32 +0100

    aggiunto forma assword dimenticata

--- js/index/formSwitch.js
+++ js/index/formSwitch.js
@@ -3,8 +3,7 @@ let formAccesso = document.getElementById("form-accesso");
 let formRegistrazione = document.getElementById("form-registrazione");
 
 
-function switchForm(vuoleRegistrarsi) {
-    console.log("switch");
-    vuoleRegistrarsi ? formRegistrazione.classList.replace("d-none", "d-block") : formAccesso.classList.replace("d-none", "d-block");
-    vuoleRegistrarsi ? formAccesso.classList.replace("d-block", "d-none") : formRegistrazione.classList.replace("d-block", "d-none");
+function switchForm(idFormVisibile, idFormDaMostrare) {
+    let form = document.getElementById(idFormDaMostrare);
+    console.log(idFormVisibile);
 }
